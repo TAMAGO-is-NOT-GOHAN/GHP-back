12456d057042f490886b52d5ba7a8e0ce3d61875
higashi000 <higashi.ict@gmail.com>
2020-02-23 08:49:47 +0900

    Add ugokukasirankedo

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 	"strconv"
 
+	firebase "firebase.google.com/go"
 	"github.com/TAMAGO-is-NOT-GOHAN/GHP-back/event"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
 )
 
 func main() {
 	router := gin.Default()
+
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: "tng-ghp-ok"}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
 
 	Test(router)
 
@@ -22,11 +39,34 @@ func Test(r *gin.Engine) {
 	})
 }
 
-func GetEvent(r *gin.Engine) {
+func GetEvent(r *gin.Engine, app *firebase.App) {
+	ctx := context.Background()
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r.GET("/v1/event", func(c *gin.Context) {
+		iter := client.Collection(c.Query("event_id")).Documents(ctx)
 		var eventData event.Event
 		eventID, _ := strconv.Atoi(c.Query("event_id"))
 		eventData.ID = uint32(eventID)
+
+		for {
+			doc, err := iter.Next()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return
+			}
+
+			doc.DataTo(&eventData)
+		}
 
 		c.JSON(http.StatusOK, eventData)
 	})
