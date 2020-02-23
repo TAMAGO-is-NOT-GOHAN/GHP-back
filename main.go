04cc9a4535a9382e1aac60a656084472180389a0
higashi000 <higashi.ict@gmail.com>
2020-02-23 16:30:00 +0900

    Add update

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
-	firebase "firebase.google.com/go"
 	"github.com/TAMAGO-is-NOT-GOHAN/GHP-back/event"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"google.golang.org/api/iterator"
 )
+
+var coors []event.Coordinate
+var ngdata []event.NG
+var users []event.User
+var eventData event.Event
 
 func main() {
 	router := gin.Default()
@@ -20,31 +22,14 @@ func main() {
 	config.AllowOrigins = []string{"https://ghp.itok01.com/*"}
 	router.Use(cors.New(config))
 
-	ctx := context.Background()
-	conf := &firebase.Config{ProjectID: "tng-ghp-ok"}
-	app, err := firebase.NewApp(ctx, conf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
 	Test(router)
-	GetEvent(router, app)
-	PostEvent(router, app)
-	PostEventJoin(router, app)
-	GetEventNgDate(router, app)
-	PostEventJoin(router, app)
-	GetEventDeparture(router)
-	GetEventRoute(router)
-	PostEventArrival(router)
-	GetEventArrivalRank(router)
-	GetUserLocation(router, app)
-	PostUserLocation(router, app)
+	GetEvent(router)
+	PostEvent(router)
+	PostEventJoin(router)
+	GetEventNgDate(router)
+	PostEventJoin(router)
+	GetUserLocation(router)
+	PostUserLocation(router)
 
 	router.Run()
 }
@@ -55,73 +40,17 @@ func Test(r *gin.Engine) {
 	})
 }
 
-func GetEvent(r *gin.Engine, app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
+func GetEvent(r *gin.Engine) {
 	r.GET("/v1/event", func(c *gin.Context) {
-		iter := client.Collection(c.Query("event_id")).Documents(ctx)
-		var eventData event.Event
-		eventData.ID = c.Query("event_id")
-
-		for {
-			doc, err := iter.Next()
-
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			doc.DataTo(&eventData)
-		}
-
-		iter = client.Collection("coor").Documents(ctx)
-
-		var coors []event.Coordinate
-
-		for {
-			doc, err := iter.Next()
-
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			var tmp event.Coordinate
-
-			doc.DataTo(&tmp)
-
-			coors = append(coors, tmp)
-		}
-
 		eventData.Coors = coors
 
 		c.JSON(http.StatusOK, eventData)
 	})
 }
 
-func PostEvent(r *gin.Engine, app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
-	var eventData event.Event
-
+func PostEvent(r *gin.Engine) {
 	r.POST("/v1/event", func(c *gin.Context) {
 		c.BindJSON(&eventData)
-
-		eventID := string(eventData.ID)
 
 		u, err := uuid.NewRandom()
 		if err != nil {
@@ -129,30 +58,13 @@ func PostEvent(r *gin.Engine, app *firebase.App) {
 		}
 
 		uuidUI := u.String()
-
-		_, _, err = client.Collection(eventID).Add(ctx, map[string]interface{}{
-			"ID":          uuidUI,
-			"Date":        eventData.Date,
-			"Name":        eventData.Name,
-			"MaxPeople":   eventData.MaxPeople,
-			"Description": eventData.Description,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
+		eventData.ID = uuidUI
 
 		c.JSON(http.StatusOK, `{"event_id":"`+uuidUI+`"}`)
 	})
 }
 
-func PostEventJoin(r *gin.Engine, app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
+func PostEventJoin(r *gin.Engine) {
 	type EventID struct {
 		EventID string `json:"event_id"`
 	}
@@ -162,173 +74,59 @@ func PostEventJoin(r *gin.Engine, app *firebase.App) {
 		user := c.Query("user")
 		c.BindJSON(&tmp)
 
-		_, _, err := client.Collection(user).Add(ctx, map[string]interface{}{
-			"ID": tmp.EventID,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
+		users = append(users, event.User{tmp.EventID, user})
 
 		c.JSON(http.StatusOK, `{"status": "ok"}`)
 	})
 }
 
-func GetEventNgDate(r *gin.Engine, app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
-	var ng []event.NG
-
+func GetEventNgDate(r *gin.Engine) {
 	r.GET("/v1/event/ngdate", func(c *gin.Context) {
-		iter := client.Collection("ng").Documents(ctx)
-
-		for {
-			doc, err := iter.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			var tmp event.NG
-
-			doc.DataTo(&tmp)
-			ng = append(ng, tmp)
-		}
-
-		c.JSON(http.StatusOK, ng)
+		c.JSON(http.StatusOK, ngdata)
 	})
 }
 
-func PostEventNgDate(r *gin.Engine, app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
+func PostEventNgDate(r *gin.Engine) {
 	var ng []event.NG
 
 	r.POST("/v1/event/ngdate", func(c *gin.Context) {
 		c.BindJSON(&ng)
 
 		for _, e := range ng {
-			_, _, err := client.Collection("ng").Add(ctx, map[string]interface{}{
-				"date": e,
-			})
-			if err != nil {
-				log.Fatal(err)
-			}
+			ngdata = append(ngdata, e)
 		}
 
 		c.JSON(http.StatusOK, `{"status": "ok"}`)
 	})
 }
 
-func GetEventDeparture(r *gin.Engine) {
-	r.GET("/v1/event/departure", func(c *gin.Context) {
-		var eventData event.Event
-		eventData.ID = c.Query("event_id")
-
-		c.JSON(http.StatusOK, eventData)
-	})
-}
-
-func GetEventRoute(r *gin.Engine) {
-	r.GET("/v1/event/route", func(c *gin.Context) {
-		var eventData event.Event
-		eventData.ID = c.Query("event_id")
-
-		c.JSON(http.StatusOK, eventData)
-	})
-}
-
-func PostEventArrival(r *gin.Engine) {
-	type EventID struct {
-		EventID string `json:"event_id"`
-	}
-	r.POST("/v1/event/departure", func(c *gin.Context) {
-		var tmp EventID
-		c.BindJSON(&tmp)
-		c.JSON(http.StatusOK, tmp)
-	})
-}
-
-func GetEventArrivalRank(r *gin.Engine) {
-	r.GET("/v1/event/arrival/rank", func(c *gin.Context) {
-		var eventData event.Event
-		eventData.ID = c.Query("event_id")
-
-		c.JSON(http.StatusOK, eventData)
-	})
-}
-
-func GetUserLocation(r *gin.Engine, app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
-	var coors []event.Coordinate
-
+func GetUserLocation(r *gin.Engine) {
 	r.GET("/v1/user/location", func(c *gin.Context) {
-		iter := client.Collection("coor").Documents(ctx)
-
-		for {
-			doc, err := iter.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				return
-			}
-
-			var tmp event.Coordinate
-
-			doc.DataTo(&tmp)
-
-			coors = append(coors, tmp)
-		}
-
 		c.JSON(http.StatusOK, coors)
 	})
 }
 
-func PostUserLocation(r *gin.Engine, app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
+func PostUserLocation(r *gin.Engine) {
 	var coor event.Coordinate
 
 	r.POST("/v1/user/location", func(c *gin.Context) {
 		c.BindJSON(&coor)
+		flg := false
 
-		_, err := client.Collection("coor").Doc(coor.User).Set(ctx, map[string]interface{}{
-			"user":      coor.User,
-			"latitude":  coor.Latitude,
-			"longitude": coor.Longitude,
-		})
+		if len(coors) == 0 {
+			for i, e := range coors {
+				if e.User == coor.User {
+					coors[i].Latitude = coor.Latitude
+					coors[i].Longitude = coor.Longitude
+					flg = true
+				}
+			}
+		}
 
-		if err != nil {
-			log.Fatal(err)
+		if !flg {
+			coors = append(coors, coor)
 		}
 
 		c.JSON(http.StatusOK, `{"status":"ok"}`)
 	})
-}
-
-func PutEvent(r *gin.Engine) {
-
 }
