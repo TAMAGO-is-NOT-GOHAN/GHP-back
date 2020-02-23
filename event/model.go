c4ac611d3c8e4cd666d8b276539a6188b8a2fbab
higashi000 <higashi.ict@gmail.com>
2020-02-23 15:59:21 +0900

    Add struct

--- event/model.go
+++ event/model.go
@@ -2,13 +2,19 @@ package event
 
 // Event ...
 type Event struct {
-	ID          string `json:"event_id"`
-	Date        uint32 `json:"date"`
-	Name        string `json:"event_name"`
-	MaxPeople   uint32 `json:"max_people"`
-	Description string `json:"description"`
+	ID          string       `json:"event_id"`
+	Date        uint32       `json:"date"`
+	Name        string       `json:"event_name"`
+	MaxPeople   uint32       `json:"max_people"`
+	Description string       `json:"description"`
+	Coors       []Coordinate `json:"coordinate"`
 }
 
 type NG struct {
 	Date uint32 `json:"date"`
+}
+
+type Coordinate struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
 }
